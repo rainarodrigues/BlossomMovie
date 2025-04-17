ad9df3be4ef7a96a061ec607b4e38dea983a353d
rainarodrigues <rainarrodrigues@gmail.com>
2025-04-17 17:41:47 -0300

    :lipstick: feat: creating home view

--- BlossomMovie/BlossomMovie/Constants.swift
+++ BlossomMovie/BlossomMovie/Constants.swift
@@ -6,12 +6,19 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct Constants {
     static let homeString = "Home"
     static let upcomingString = "Upcoming"
     static let searchString = "Search"
     static let downloadString = "Download"
+    static let playStrig = "Play"
+    static let trendingMovieString = "Trending Movies"
+    static let trendingTVString = "Trending TV"
+    static let topRatedMovieString = "Top Rated Movies"
+    static let topRatedTVString = "Top Rated TV"
+    
     
     static let homeIconString = "house"
     static let upcomingIconString = "play.circle"
@@ -19,4 +26,19 @@ struct Constants {
     static let downloadIconString = "arrow.down.to.line"
     
     static let testTitleURL = "https://image.tmdb.org/t/p/w500/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg"//https://image.tmdb.org/t/p/w500/nn160WkyPpuMm595hmAxNw3rZFn.jpg
+    static let testTitleURL2 = "https://image.tmdb.org/t/p/w500/d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg"
+    static let testTitleURL3 = "https://image.tmdb.org/t/p/w342/d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg"
+}
+
+extension Text {
+    func ghostButton() -> some View {
+        self
+            .frame(width: 100, height: 50)
+            .foregroundStyle(.buttonText)
+            .bold()
+            .background{
+                RoundedRectangle(cornerRadius: 20, style: .continuous)
+                    .stroke(.buttonBorder, lineWidth: 5)
+            }
+    }
 }
--- BlossomMovie/BlossomMovie/HomeView.swift
+++ BlossomMovie/BlossomMovie/HomeView.swift
@@ -11,13 +11,49 @@ struct HomeView: View {
     var heroTestTitle: String = Constants.testTitleURL
     
     var body: some View {
-        VStack{
-            AsyncImage(url: URL(string: heroTestTitle)) { image in
-                image
-                    .resizable().scaledToFit()
-            } placeholder: {
-                ProgressView()
+        GeometryReader{ geo in
+            ScrollView{
+                LazyVStack{
+                    VStack(){
+                        AsyncImage(url: URL(string: heroTestTitle)) { image in
+                            image
+                                .resizable()
+                                .scaledToFit()
+                                .overlay{
+                                    LinearGradient(
+                                        stops: [Gradient.Stop(color: .clear, location: 0.8),
+                                                Gradient.Stop(color: .gradient, location: 1)],
+                                        startPoint: .top, endPoint: .bottom
+                                    )
+                                }
+                        } placeholder: {
+                            ProgressView()
+                        }.frame(width: geo.size.width, height: geo.size.height * 0.85)
+                        HStack{
+                            Button{
+                                
+                            }label: {
+                                Text(Constants.playStrig)
+                                    .ghostButton()
+                            }
+                            
+                            Button{
+                                
+                            }label: {
+                                Text(Constants.downloadString)
+                                    .ghostButton()
+                            }
+                        }
+                        HorizontalListView(header: Constants.trendingTVString)
+                        HorizontalListView(header: Constants.trendingMovieString)
+                        HorizontalListView(header: Constants.topRatedMovieString)
+                        HorizontalListView(header: Constants.topRatedTVString)
+                        
+                    }
+                    .border(Color.red)
+                }
             }
+            .border(Color.green)
         }
     }
 }
